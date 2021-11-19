884ed4e1cbfc79df9db36fa0fbd4f97694f82e23
Hugo Averty <hugoaverty@gmail.com>
2021-11-19 09:17:00 -0400

    Adding typo, popin, and custom link

---
+++ facts.js
@@ -0,0 +1,20 @@
+WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+
+let fact1Popup;
+
+// Open the popup when we enter a given zone
+fact1Popup = WA.room.onEnterZone('fact1Zone', () => {
+    WA.ui.openPopup('popin', 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+// Close the popup when we leave the zone.
+WA.room.onLeaveZone('fact1Zone', () => {
+    fact1Popup.close();
+});
